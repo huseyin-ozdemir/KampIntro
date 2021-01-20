6f1f8a11d8e7a56a332134e74572aa4e191fd3f5
huseyinozdemir13@yaani.com <huseyinozdemir13@yaani.com>
2021-01-20 21:59:10 +0300

    Hey guys

---
+++ Donguler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kur1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Proglamlamaya Başlangıç Kursu";
+            string kurs3 = "JAVA";
+
+            //array - dizi
+            string[] kurslar = new string[] {"C#", "Python", "Yazılım Geliştirici Yetiştirme Kampı", "Proglamlamaya Başlangıç Kursu", "JAVA" };
+
+
+
+            for (int i = 0; i<kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("__________");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);            }
+        }
+    }
+}
---
+++ KampIntro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace KampIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kategoriEtiketi = "Kategori";
+            int ogrenciSayisi = 320000;
+            double faizOrani = 1.45;
+            bool sistemeGirisYapmismi = true;
+            double dolarDun = 7.55;
+            double dolarBugun = 7.56;
+            //dolar hesap
+            if (dolarDun<dolarBugun)
+            {
+                Console.WriteLine("Artış Butonu");
+
+            }
+            else if (dolarBugun<dolarDun)
+            {
+                Console.WriteLine("Azalış Butonu");
+            }
+            else
+            {
+                Console.WriteLine("Değişmedi butonu");
+            }
+
+            //sistem girişi
+            if (sistemeGirisYapmismi == true)
+            {
+                Console.WriteLine("Kullanıcı ayarları butonu");
+            }
+            else
+            {
+                Console.WriteLine("sistemeGirisYapmismi yap butonu");
+            }
+            
+        }
+    }
+}
