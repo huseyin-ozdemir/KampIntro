e5663e5f8c9ec8219ba965e9ad4a26bb88339d1f
huseyinozdemir13@yaani.com <huseyinozdemir13@yaani.com>
2021-01-23 23:56:16 +0300

    Heyy

---
+++ OOP3/BasvuruManger.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    class BasvuruManger
+    {
+        //Method injection
+        //List yapıp birden fazla loglama yapabilirsin.
+        public void BasvuruYap(IKrediManager krediManager, ILoggerService loggerService)
+        {
+            //Başvuru bilgileri değerlendirme ...
+
+            //Tek bir manageer a  bağımlı olmadı hepsini referans verdik 
+            //Hangi krediyi gönderirsen o çalışır..
+
+            krediManager.Hesapla();
+            loggerService.Log();        }
+        
+        public void KrediOnBilgilendirmesiYap(List<IKrediManager> krediler)
+        {
+            foreach (var kredi in krediler)
+            {
+                kredi.Hesapla();
+                
+            }
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyazKrediManager = new IhtiyacKrediManager();
+            
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService dataBaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileLoggerService = new FileBaseLoggerService();
+
+
+            BasvuruManger basvuruManger = new BasvuruManger();
+            basvuruManger.BasvuruYap(new EsnafKrediManager(), fileLoggerService);
+
+            //Listeden başvuruya uyan kredileri seçip gösterir.
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyazKrediManager, tasitKrediManager };
+            //basvuruManger.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
